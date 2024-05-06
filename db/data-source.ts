4e53e4cf2fe6eb786c14da2f124fd3df13516edc
lunaiden <melanie.leeman@protonmail.com>
2024-05-06 14:22:49 +0200

    trying to fix ssl error vercel (with postgres url)

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,22 +1,23 @@
 import * as dotenv from 'dotenv';
 import * as dotenvExpand from 'dotenv-expand';
 import { DataSource, DataSourceOptions } from 'typeorm';
+import * as fs from 'node:fs';
 dotenvExpand.expand(dotenv.config());
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.POSTGRES_HOST,
+  url: process.env.POSTGRES_URL,
+  // host: process.env.POSTGRES_HOST,
   // host: 'localhost',
-  port: 5432,
-  username: process.env.POSTGRES_USER,
+  // port: 5432,
+  // username: process.env.POSTGRES_USER,
   // username: 'postgres',
-  password: process.env.POSTGRES_PASSWORD,
+  // password: process.env.POSTGRES_PASSWORD,
   // password: 'postgres',
-  database: process.env.POSTGRES_DATABASE,
+  // database: process.env.POSTGRES_DATABASE,
   // database: 'gsbackoffice',
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/db/migrations/*.js'],
-  // synchronize: true,
 };
 
 const dataSource = new DataSource(dataSourceOptions);

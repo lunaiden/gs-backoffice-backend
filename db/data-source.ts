1ef465424980fe11307910ef57de335e83cba49a
lunaiden <melanie.leeman@protonmail.com>
2024-05-06 14:53:21 +0200

    try to run migrations during vercel deployment

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,21 +1,11 @@
 import * as dotenv from 'dotenv';
 import * as dotenvExpand from 'dotenv-expand';
 import { DataSource, DataSourceOptions } from 'typeorm';
-import * as fs from 'node:fs';
 dotenvExpand.expand(dotenv.config());
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   url: process.env.POSTGRES_URL,
-  // host: process.env.POSTGRES_HOST,
-  // host: 'localhost',
-  // port: 5432,
-  // username: process.env.POSTGRES_USER,
-  // username: 'postgres',
-  // password: process.env.POSTGRES_PASSWORD,
-  // password: 'postgres',
-  // database: process.env.POSTGRES_DATABASE,
-  // database: 'gsbackoffice',
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/db/migrations/*.js'],
 };

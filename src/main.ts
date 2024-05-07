6272b3d7e88963fd09d47659da5e13164dec84dc
lunaiden <melanie.leeman@protonmail.com>
2024-05-07 16:58:10 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,13 @@ async function bootstrap() {
   config();
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: process.env.FRONT_URL, credentials: true });
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', process.env.FRONT_URL);
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);

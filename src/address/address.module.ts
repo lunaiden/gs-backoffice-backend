73e3e1401ce600d231569a1fe9adb4e072748cb4
lunaiden <melanie.leeman@protonmail.com>
2024-05-09 14:22:33 +0200

    add company entity to address module

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { AddressController } from './address.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Company } from '../company/entities/company.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Company])],
   controllers: [AddressController],
   providers: [AddressService],
 })

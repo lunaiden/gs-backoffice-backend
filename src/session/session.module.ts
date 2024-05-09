181a4456c15e7ba8033404a01f5d1aa4925f08c6
lunaiden <melanie.leeman@protonmail.com>
2024-05-09 14:55:58 +0200

    test3

--- src/session/session.module.ts
+++ src/session/session.module.ts
@@ -4,9 +4,11 @@ import { SessionController } from './session.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Result } from '../auth/entities/result.entity';
 import { SessionMember } from './entities/session_member.entity';
+import { Session } from './entities/session.entity';
+import { User } from '../auth/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Result, SessionMember])],
+  imports: [TypeOrmModule.forFeature([Result, SessionMember, Session, User])],
   controllers: [SessionController],
   providers: [SessionService],
 })

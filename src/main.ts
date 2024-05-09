1b283d892637bf89e5e8ae9d3a1c17fd3440a431
lunaiden <melanie.leeman@protonmail.com>
2024-05-09 15:19:52 +0200

    test4

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,15 @@ import cookieParser = require('cookie-parser');
 async function bootstrap() {
   config();
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: process.env.FRONT_URL, credentials: true });
+  app.enableCors({
+    origin: [
+      process.env.FRONT_URL,
+      'http://localhost:5173',
+      '82.67.24.9:5173',
+      '82.67.24.9:8080',
+    ],
+    credentials: true,
+  });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
